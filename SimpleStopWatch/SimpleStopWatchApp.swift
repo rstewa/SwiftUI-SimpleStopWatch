b671209dd41c6ecf912f175ddfb523273631ada2
rstewa <70178227+rstewa@users.noreply.github.com>
2024-10-14 12:40:44 -0500

    Initial Commit

---
+++ SimpleStopWatch/SimpleStopWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleStopWatchApp.swift
+//  SimpleStopWatch
+//
+//  Created by Ryan Stewart on 10/14/24.
+//
+
+import SwiftUI
+
+@main
+struct SimpleStopWatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
